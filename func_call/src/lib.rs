7749ba0fcf9407886d2e5c6df5903ad8aef95eaf
MudassirAhmad <mudassirahmad998@gmail.com>
2020-01-19 14:00:12 +0500

    Question 3 Of Assignment 7

---
+++ func_call/src/lib.rs
@@ -0,0 +1,7 @@
+pub mod uni{
+    pub mod section{
+        pub fn section_name(){
+            println!("This is section 4B");
+        }
+    }
+}
---
+++ src/main.rs
@@ -0,0 +1,4 @@
+use func_call;
+fn main() {
+    func_call::uni::section::section_name();
+}
